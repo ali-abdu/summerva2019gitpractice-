2f72c328dd9248751601fad056aed44373350bea
ali-abdu <55106797+ali-abdu@users.noreply.github.com>
2019-11-21 16:43:02 -0500

    just small change

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,7 +1,7 @@
 public class FirstClass {
 
     public static void main(String[] args) {
-        System.out.println("hi Ali);
+        System.out.println("hi Ali working till 5);
         
     }
 
